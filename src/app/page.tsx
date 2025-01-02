c2fbba96bde471187313de53ab5b20738756c9ce
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2025-01-02 11:38:55 -0500

    implementing konva for interactive canvas to add circles and rectangles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,149 @@
-import Image from "next/image";
-
+"use client";
+import { createContext, useEffect, useState, useRef } from "react";
+import { Stage, Layer, Circle, Rect } from "react-konva";
+import Konva from "konva";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  //initializes dimensions
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  //initializes all state
+  const [shapes, setShapes] = useState([]);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [shapeType, setShapeType] = useState(null);
+  const [point1, setPoint1] = useState({
+    x1: 0,
+    y1: 0,
+  });
+  const [point2, setPonit2] = useState({
+    x2: 0,
+    y2: 0,
+  });
+  const layerRef = useRef(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  useEffect(() => {
+    // Set the canvas size after the component mounts (client-side)
+    setDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
+
+  const handleMouseDown = (e) => {
+    //grab latest mouse position
+    const pos = e.target.getStage().getPointerPosition();
+    point1.x1 = pos.x;
+    point1.y1 = pos.y;
+    setIsDrawing(true);
+    console.log(point1);
+  };
+
+  const handleMouseMove = (e) => {
+    //keeping in case we want to customize by dragging out
+    if (!isDrawing) return;
+    const pos = e.target.getStage().getPointerPosition();
+    point2.x2 = pos.x;
+    point2.y2 = pos.y;
+    console.log(point2);
+  };
+
+  const handleMouseUp = (e) => {
+    //once we let release/unclick we create the shape
+    setIsDrawing(false);
+    console.log(point1, point2);
+    const shape = createShape(point1, point2);
+    //add it our shapes
+    setShapes((prevShapes) => [...prevShapes, shape]);
+  };
+
+  const createShape = (p1, p2) => {
+    let distanceP1toP2 = Math.sqrt((p2.x2 - p1.x1) ** 2 + (p2.y2 - p1.y1) ** 2);
+    let shape;
+    if (shapeType === "circle") {
+      shape = new Konva.Circle({
+        id: Date.now().toString(),
+        radius: distanceP1toP2,
+        fill: "red",
+        stroke: "black",
+        strokeWidth: 5,
+        x: p1.x1,
+        y: p1.y1,
+      });
+    } else if (shapeType === "rectangle") {
+      shape = new Konva.Rect({
+        id: Date.now().toString(),
+        height: p2.y2 - p1.y1,
+        width: p2.x2 - p1.x1,
+        fill: "blue",
+        stroke: "black",
+        strokeWidth: 5,
+        x: p1.x1,
+        y: p1.y1,
+      });
+    }
+    console.log(shape);
+    return shape;
+  };
+
+  const handleShapeType = (shape) => {
+    setShapeType(shape);
+  };
+  return (
+    <div>
+      <div>
+        <p>Write or draw a letter to a climate changer:</p>
+        <button
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+          onClick={() => handleShapeType("circle")}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Add Circle
+        </button>
+        <button
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+          onClick={() => handleShapeType("rectangle")}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Add Rectangle
+        </button>
+      </div>
+      <Stage
+        width={dimensions.width}
+        height={dimensions.height}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        {/* stage is a div wrapper */}
+        <Layer ref={layerRef}>
+          {/* layer is 2d canvas element */}
+          {/* Render all shapes */}
+          {shapes.map((shape) => {
+            if (shape instanceof Konva.Circle) {
+              return (
+                <Circle
+                  key={shape.attrs.id}
+                  radius={shape.attrs.radius}
+                  fill={shape.attrs.fill}
+                  stroke={shape.attrs.stroke}
+                  strokeWidth={shape.attrs.strokeWidth}
+                  x={shape.attrs.x}
+                  y={shape.attrs.y}
+                />
+              );
+            } else if (shape instanceof Konva.Rect) {
+              return (
+                <Rect
+                  key={shape.attrs.id}
+                  width={shape.attrs.width}
+                  height={shape.attrs.height}
+                  fill={shape.attrs.fill}
+                  stroke={shape.attrs.stroke}
+                  strokeWidth={shape.attrs.strokeWidth}
+                  x={shape.attrs.x}
+                  y={shape.attrs.y}
+                />
+              );
+            }
+          })}
+        </Layer>
+      </Stage>
     </div>
   );
 }
